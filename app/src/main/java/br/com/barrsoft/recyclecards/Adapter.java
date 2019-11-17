b2331972da4e1cf1db2b77c1892fdd0262afaace
Reginaldo Barreto <reginaldo.barreto@gmail.com>
2019-11-16 21:18:37 -0300

    add RecylerAdapter

---
+++ app/src/main/java/br/com/barrsoft/recyclecards/Adapter.java
@@ -0,0 +1,26 @@
+package br.com.barrsoft.recyclecards;
+
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class Adapter extends RecyclerView.Adapter<VHProject> {
+
+
+
+    @NonNull
+    @Override
+    public VHProject onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull VHProject holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
